f0c4b643610e8d4db50405c7bfeb0fa656492623
Manish <manish119427@gmail.com>
2023-12-29 16:52:24 +0530

    new

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
